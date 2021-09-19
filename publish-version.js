45d6a9dc60651799d1180d98ff1656813c331759
파링 <parangee9706@gmail.com>
2021-09-19 22:30:58 +0900

    dev publish action

---
+++ publish-version.js
@@ -0,0 +1,9 @@
+// DO NOT RUN THIS ON DEV ENVIRONMENT
+// **CI ONLY**
+
+const pkg = require('./package.json')
+const fs = require('fs')
+
+pkg.version = pkg.version + '-dev.' + process.env.COMMIT_ID
+
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 4))
